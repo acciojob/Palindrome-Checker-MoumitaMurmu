1343ee84733c3d8b30d4a040147cd1d0ffcd8b7b
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2022-12-14 16:25:04 +0530

    initial commit

--- solution.js
+++ solution.js
@@ -1,6 +1,18 @@
 // complete the given function
 
+  
+  
+  
+ 
 function palindrome(str){
-
+ var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+  var checkPalindrome = removeChar.split('').reverse().join('');
+   if(removeChar === checkPalindrome){
+       return true;
+     }else{
+       return false;
+     }
+  }
+palindrome("racecar");
 }
 module.exports = palindrome
